309af64a1b8e943d1474a5faca47b0ed08910a64
二場 昴輔 <futaba@futaba-mac-mini.local>
2024-12-04 23:19:17 +0900

    2024.12.04

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 # zshrc
-ln -s ./.zshrc ~
-curl -fsSL https://starship.rs/install.sh | bash
+ln -s $(pwd)/.zshrc ~
+curl -fsSL https://starship.rs/install.sh | sh
 
 # vim
-ln -s ./.vimrc ~
+ln -s $(pwd)/.vimrc ~
 
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.vim/indent
 mkdir -p ~/.vim/colors
 
-cp ./.vim_files/c.vim ~/.vim/indent/c.vim
-cp ./.vim_files/python.vim ~/.vim/indent/python.vim
+cp $(pwd)/.vim_files/c.vim ~/.vim/indent/c.vim
+cp $(pwd)/.vim_files/python.vim ~/.vim/indent/python.vim
 
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
